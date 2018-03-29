be98fd78ed783f5885626f6d7f8380341ee63b69
Emiliya <vuntsova@aol.com>
2018-03-29 18:56:47 -0400

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -1,52 +1,49 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-    let board = [];
+  const board = [];
 
-    for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
-        const row = [];
-        for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
-            row.push(' ');
-        };
-        board.push(row);
-    };
-    return board;
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
-const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) =>{
-    let board = [];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
 
-    for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
-        let row = [];
-        for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
-            row.push(null);
-        }
-        board.push(row);
-    };
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 
+  let numberOfBombsPlaced = 0;
 
-    let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // This code has the potential to place bombs on top of bombs, this will be fixed with control flow.
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
 
-        while(numberOfBombsPlaced < numberOfBombs){
-            // The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
-            let randomRowIndex = Math.floor(Math.random() * numberOfRows);
-            let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-
-            board[randomRowIndex][randomColumnIndex] = 'B';
-            numberOfBombsPlaced++;
-        };
-
-
-    return board;
+  return board;
 };
 
-const printBoard = board =>{
-    //board.map(row => row.join(' | ')).join('\n');
-    console.log(board.map(row => row.join(' | ')).join('\n'));
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
-let playerBoard = generatePlayerBoard(5,6);
-let bombBoard = generateBombBoard(5,6,8);
-console.log("Player Board");
+let playerBoard = generatePlayerBoard(3, 3);
+let bombBoard = generateBombBoard(3, 3, 3);
+console.log('Player Board:');
 printBoard(playerBoard);
-
-console.log("Bomb Board");
+console.log('Bomb Board:');
+// bombBoard will sometimes have less bombs than specified due to the previously-mentioned missing code.
+// Additionally, printing bombBoard will not look clean due to use of null instead of ' ' - this should just be for debugging, not presentation.
 printBoard(bombBoard);
